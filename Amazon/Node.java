cf87be4ecfc53d2fd2cf8b8a1e0108ecbd402a58
LearningSatty <satty.ads@gmail.com>
2017-01-05 18:42:57 +0530

    Create Node.java

---
+++ Amazon/Node.java
@@ -0,0 +1,16 @@
+package com.satty.amazon;
+
+
+public class Node{
+	int data;
+	Node left;
+	Node right;
+	public Node(int value){
+		this.data = value;
+		//left = right = null;
+	}
+	
+	public void insert(int value){
+		
+	}
+}
